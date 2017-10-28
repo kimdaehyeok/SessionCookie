e234af82d5b1c635a7d51fcee7e929ba02b2dae0
Daehyeok Kim <yeriel9159@gmail.com>
2017-10-28 23:17:51 +0900

    * HttpSession setAttribute, getAttribute 실습 * ClassPath 실습

--- SessionCookie/src/main/java/com/session/cookie/handler/RequestHandler.java
+++ SessionCookie/src/main/java/com/session/cookie/handler/RequestHandler.java
@@ -1,8 +1,6 @@
 package com.session.cookie.handler;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.session.cookie.sessions.HttpSessions;
 
@@ -28,9 +27,14 @@ public class RequestHandler extends HttpServlet
         if (paramName.equals(name1) && paramPass.equals(password1))
         {
             System.out.println("GET HEADER : " + request.getHeader("Cookie"));
-            
+             
             HttpSessions.inputCookie(request.getHeader("Cookie"), request.getSession());
             response.addHeader("Set-Cookie",request.getHeader("Cookie"));
+
+            HttpSession session = request.getSession();
+            session.setAttribute("sessionName", paramName + "is name of session");
+            
+            System.out.println("Set Attribute Name : " + session.getAttribute("sessionName"));
             
             RequestDispatcher requestDispatcher = request.getRequestDispatcher("/jsp/success.jsp");
             requestDispatcher.forward(request, response);
